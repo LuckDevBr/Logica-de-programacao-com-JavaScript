5bd768ca779cf31688764de04e3079fc1b9b70fc
Lucas <lucas111machado.lm@gmail.com>
2026-03-31 10:36:33 -0300

    Dica no console

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //INICIO DO JOGO ENCONTRE O N° SECRETO
 alert("Boas vindas ao jogo do n° secreto");
  let numeroSecreto = 5;
+ console.log(numeroSecreto);
+ console.log("Vem depois do n° 4");
  let chute = prompt("Escolha um número entre 1 a 10");
 
-//MENSAGEM CASO VC ENCONTRE O N° SECRETO
+//MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
    if(chute == numeroSecreto){
      console.log("Isso aí! você descobriu o n° secreto (5)");
 alert("Parabéns, este é n° secreto");
