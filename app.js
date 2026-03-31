780a3d3f4984b2a8982c139d56e236b9b0f82578
Lucas <lucas111machado.lm@gmail.com>
2026-03-31 10:38:27 -0300

    Mudando a ordem do console

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //INICIO DO JOGO ENCONTRE O N° SECRETO
 alert("Boas vindas ao jogo do n° secreto");
  let numeroSecreto = 5;
- console.log(numeroSecreto);
- console.log("Vem depois do n° 4");
+    console.log("Vem depois do n° 4");
+    console.log(numeroSecreto);
  let chute = prompt("Escolha um número entre 1 a 10");
 
 //MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
-   if(chute == numeroSecreto){
+    if(chute == numeroSecreto){
      console.log("Isso aí! você descobriu o n° secreto (5)");
 alert("Parabéns, este é n° secreto");
 }   else{
