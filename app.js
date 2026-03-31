2d34601983e125a6b6e617e0c61f3301901a2a12
Lucas <lucas111machado.lm@gmail.com>
2026-03-31 10:16:38 -0300

    acertei o numero secreto
    (//) barra dupla para inserir comentários
    (if) caso vc encontrar aparecerá uma mensagem na tela e no console.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-alert("Boas vindas ao jogo do número secreto");
+//INICIO DO JOGO ENCONTRE O N° SECRETO
+alert("Boas vindas ao jogo do n°secreto");
 let numeroSecreto = 5;
 let chute = prompt("Escolha um número entre 1 a 10");
+
+//MENSAGEM CASO VC ENCONTRE O N° SECRETO
+if(chute == numeroSecreto){
+    console.log("Isso aí! você descobriu po n° secreto (5)");
+    alert("Parabéns, este é n° secreto");
+}
