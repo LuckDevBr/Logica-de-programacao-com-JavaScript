fa97485a40b8b4e3ea37bcfea93a61ba9dc43471
Lucas <lucas111machado.lm@gmail.com>
2026-03-31 10:30:50 -0300

    Esse não é o n° secreto
    numero errado irá aparecer tanto na tela quanto no console

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //INICIO DO JOGO ENCONTRE O N° SECRETO
 alert("Boas vindas ao jogo do n° secreto");
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 a 10");
+ let numeroSecreto = 5;
+ let chute = prompt("Escolha um número entre 1 a 10");
 
 //MENSAGEM CASO VC ENCONTRE O N° SECRETO
-if(chute == numeroSecreto){
-    console.log("Isso aí! você descobriu o n° secreto (5)");
-    alert("Parabéns, este é n° secreto");
-} 
+   if(chute == numeroSecreto){
+     console.log("Isso aí! você descobriu o n° secreto (5)");
+alert("Parabéns, este é n° secreto");
+}   else{
+     console.log("Não fique triste, tente novamente")
+alert("Você errou :(");
+}
