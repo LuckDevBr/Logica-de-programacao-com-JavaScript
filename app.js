01500f5a86fa591dfdd2fb3122d49566d007b35d
Lucas <lucas111machado.lm@gmail.com>
2026-04-02 01:02:59 -0300

    Retornando ao projeto da aula

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-let carta = prompt ("Qual o n° da sua carta?");
-let numeroDaCarta = -1;
-if(carta <= numeroDaCarta){
-    alert("negativo");
-} else{
-    alert("positivo");
+//INICIO DO JOGO ENCONTRE O N° SECRETO
+alert("Boas vindas ao jogo do n° secreto");
+ let numeroSecreto = 1;
+    console.log("DICA");
+    console.log(numeroSecreto);
+ let chute = prompt("Escolha um número entre 1 a 10");
+//MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
+    if(chute == numeroSecreto){
+     console.log("Isso aí! você descobriu o n° secreto");
+alert(`Parabéns, este é n° secreto ${numeroSecreto}`);
+}   else{
+     console.log("Não fique triste, tente novamente")
+alert("Você errou :(");
 }
