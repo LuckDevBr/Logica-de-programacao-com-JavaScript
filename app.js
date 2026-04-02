fe43782ce12773e07d972185d589393b671ce4bc
Lucas <lucas111machado.lm@gmail.com>
2026-04-01 21:28:27 -0300

    Combinando texto com valores de variáveis

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //INICIO DO JOGO ENCONTRE O N° SECRETO
 alert("Boas vindas ao jogo do n° secreto");
- let numeroSecreto = 5;
-    console.log("Vem depois do n° 4");
+ let numeroSecreto = 3;
+    console.log("DICA");
     console.log(numeroSecreto);
  let chute = prompt("Escolha um número entre 1 a 10");
 
 //MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
     if(chute == numeroSecreto){
-     console.log("Isso aí! você descobriu o n° secreto (5)");
+     console.log(´Isso aí! você descobriu o n° secreto ${numeroSecreto}´);
 alert("Parabéns, este é n° secreto");
 }   else{
      console.log("Não fique triste, tente novamente")
