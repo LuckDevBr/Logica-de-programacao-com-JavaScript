55ad68b246877444ebc3ea520b520ca960d5e885
Lucas <lucas111machado.lm@gmail.com>
2026-04-02 00:17:44 -0300

    3° Mini Desafio Alura

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-//Jogos de pontos
-console.log("Boa sorte!");
-alert("Bem vindo ao jogo de pontos");
+alert("Saldo atual da sua conta");
+let saldo = 1000;
 
-//Ponto minimo para ganhar e pergunta de quantos pontos voce fez
-console.log("Tente fazer do que 99 pontos")
-let pontosV = 100;
-let pontuação = prompt ("Quantos pontos você fez?");
-
-//Mensagem caso ganhe ou perca
-if( pontuação >= pontosV){
-    console.log("Rei dos pontos");
-    alert("Parabéns, você venceu!");
-} else{
-    console.log("Na proxima você vai conseguir!");
-    alert("Tente novamente para ganhar");
-}
+alert(`R$ ${saldo}`);
