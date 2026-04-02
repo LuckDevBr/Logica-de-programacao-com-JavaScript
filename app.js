edbdecbf9d70cbd5c7951e8ae4c1abb26ddb10f2
Lucas <lucas111machado.lm@gmail.com>
2026-04-02 19:09:47 -0300

    organizações e novas condições adicionadas

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-//INICIO DO JOGO ENCONTRE O N° SECRETO
+// INICIO DO JOGO ENCONTRE O N° SECRETO
 alert("Boas vindas ao jogo do n° secreto");
- let numeroSecreto = 1;
-    console.log("DICA");
-    console.log(numeroSecreto);
- let chute = prompt("Escolha um número entre 1 a 10");
-//MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
-    if(chute == numeroSecreto){
-     console.log("Isso aí! você descobriu o n° secreto");
-alert(`Parabéns, este é n° secreto ${numeroSecreto}`);
-}   else{
-     console.log("Não fique triste, tente novamente")
-alert("Você errou :(");
+
+let numeroSecreto = 4;
+console.log("DICA: " + numeroSecreto);
+
+let chute = prompt("Escolha um número entre 1 a 10");
+
+// MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
+if (chute == numeroSecreto) {
+    console.log("Isso aí! você descobriu o n° secreto");
+    alert(`Parabéns, este é o n° secreto: ${numeroSecreto}`);
+} else {
+    // Lógica para quando o chute for diferente do número secreto
+    if (chute > numeroSecreto) {
+        alert(`O n° secreto é menor que ${chute}`);
+    } else {
+        alert(`O n° secreto é maior que ${chute}`);
+    }
 }
