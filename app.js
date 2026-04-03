b4839e581d3132ed3bd0377360ee35a3d38520bc
Lucas <lucas111machado.lm@gmail.com>
2026-04-03 19:54:52 -0300

    Aprendendo como adicionar a quantidade de tentativas.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 alert("Seja bem vindo ao jogo do número secreto");
 
-let chute
-let numeroSecreto = 5;
+let chute;
+let numeroSecreto = 777;
+let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt ("Qual é o número secreto?");
+    chute = prompt("Qual é o número secreto entre 1 a 1000?");
     if (chute == numeroSecreto) {
-        alert(`Parabens! O número é ${chute}`);
+        alert(`Parabens! O número secreto é ${chute} depois de ${tentativas} tentivas`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número é menor do que ${chute}`);
         } else {
             alert(`O número é maior do que ${chute}`);
         }
+        tentativas++;
     }
 }
