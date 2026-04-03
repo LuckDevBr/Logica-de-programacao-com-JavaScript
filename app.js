f2ecaed20d0ea389da56ad67abf30a00cc09e43b
Lucas <lucas111machado.lm@gmail.com>
2026-04-03 18:49:09 -0300

    Praticando a função while com (if/else)

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-// INICIO DO JOGO ENCONTRE O N° SECRETO
-alert("Boas vindas ao jogo do n° secreto");
+alert("Seja bem vindo ao jogo do número secreto");
 
-let numeroSecreto = 4;
-console.log("DICA: " + numeroSecreto);
+let chute
+let numeroSecreto = 5;
 
-let chute = prompt("Escolha um número entre 1 a 10");
-
-// MENSAGEM SE VC ENCONTRAR OU NÃO ENCONTRAR O N°
-if (chute == numeroSecreto) {
-    console.log("Isso aí! você descobriu o n° secreto");
-    alert(`Parabéns, este é o n° secreto: ${numeroSecreto}`);
-} else {
-    // Lógica para quando o chute for diferente do número secreto
-    if (chute > numeroSecreto) {
-        alert(`O n° secreto é menor que ${chute}`);
+while (chute != numeroSecreto) {
+    prompt = ("Qual é o número secreto?");
+    if (chute == numeroSecreto) {
+        alert(`Parabens! ${chute}`);
     } else {
-        alert(`O n° secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número é menor do que ${chute}`);
+        } else {
+            alert(`O número menor do que ${chute}`);
+        }
     }
 }
