7dda07db97c897976b825e3966b604d393aa2d60
Lucas <lucas111machado.lm@gmail.com>
2026-04-08 11:15:13 -0300

    Loop inifinto

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let contador = 1;
-
-while (contador < 4) {
-  console.log('Executando a interação ' + contador);
-  contador = contador + 1;
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
+while (contador > 0) {
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
 }
+let media = soma / qtdNumeros;
+console.log(media);
