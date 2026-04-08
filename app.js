e5992d7e6f1c4ec9dd6f4bea90a8071cd0e2b2f7
Lucas <lucas111machado.lm@gmail.com>
2026-04-08 11:26:37 -0300

    Desafio 1
    começa em 1 e vá até 10 usando um loop while

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-let qtdNumeros = parseInt(prompt('Digite a quantidade de números para o cálculo da média:'));
-let soma = 0;
-let contador = qtdNumeros;
-
-while (contador > 0) {
-    let numero = parseInt(prompt('Digite o numero:'));
-    soma += numero;
-    contador--; 
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
 }
-
-let media = soma / qtdNumeros;
-console.log(`A média é: ${media}`);
