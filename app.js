a6c07a1eddaa8e2dc01716f39c9c459481ba30b7
Lucas <lucas111machado.lm@gmail.com>
2026-06-17 11:27:53 -0300

    adicionando comentarios no projeto

--- app.js
+++ app.js
@@ -1,56 +1,26 @@
-console.log("Boas vindas!");
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute;
+let tentativas = 1; // o chute é a tentativa do jogador, o número secreto é o número que o jogador tem que acertar, e as tentativas é a quantidade de vezes que o jogador tentou acertar o número secreto
 
-let name = "Lucas";
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e 100`);
 
-alert(`Hello, ${name}!`);
+    //se chute for igual o numero secreto
+    if (chute == numeroSecreto) {
+        break; // o break para o código, ou seja, para o laço de repetição
+    
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número é menor que ${chute}`);
 
-prompt("Qual a linguagem de programação que você mais gosta?");
+        } else (chute < numeroSecreto); {
+            alert(`O número é maior que ${chute}`);
+        }
 
-let valor1 = 7;
-let valor2 = 4;
-let resultado = valor1 + valor2;
-
-console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
-
-let v1 = 10;
-let v2 = 5;
-let resultado2 = v1 - v2;
-
-console.log(`A diferença entre ${v1} e ${v2} é igual a ${resultado2}`);
-
-let idade = prompt("Digite sua idade:");
-
-if (idade >= 18) {
-    console.log("Você é maior de idade.");
-} else {
-    console.log("Você não é maior de idade.");
+        tentativas++; // o ++ é um operador de incremento, ou seja, ele aumenta o valor da variável em 1
+    }
 }
-var numero = parseFloat(prompt("Digite um número:"));
-
-if (numero > 0) {
-    console.log("O numero é positivo.");
-} else if (numero < 0) {
-    console.log("O numero é negativo.");
-} else {
-    console.log("O numero é zero.");
-}
-
-let numero1 = 1;
-while (numero1 <= 10) {
-    console.log(numero1);
-    numero1++;
-}
-let nota1 = prompt("Digite a primeira nota:");
-if (nota1 >= 7) {
-    alert("Aprovado!");
-} else {
-    alert("Reprovado!");
-}
-
-let numeroAleatorio = Math.random();
-console.log(numeroAleatorio);
-
-let numeroInteiroAleatorio = parseInt(Math.random() * 10) + 1;
-console.log(numeroInteiroAleatorio);
-let numeroInteiroAleatorio = parseInt(Math.random() * 1000) + 1;
-console.log(numeroInteiroAleatorio);
+// se for 1 tentativa, palavra tentiva, se for mais de 1, palavra tentativas
+let palavraTentativa = tentativas > 1 ? "tentiva" : "tentativas";
